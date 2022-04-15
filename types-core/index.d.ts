36ea371c9187d94b82207552f072991935db1c47
Eric Jizba <erijiz@microsoft.com>
2022-04-15 16:32:04 -0700

    simplify core types

--- types-core/index.d.ts
+++ types-core/index.d.ts
@@ -1,19 +1,16 @@
-/**
- * This module will be a built-in part of the worker, similar to how Node.js provides built-in modules like "http" or "path" at runtime
- * It will not be available on npm and will not be listed in a user's package.json, but we could provide a "@types/azure__functions-worker" package for it
- * The goal of this module is to be very barebones and closely match the types the worker uses to communicate with the host over the rpc channel
- */
 declare module '@azure/functions-core' {
     export interface FunctionMetadata {
         name: string;
 
         /**
-         * An array containing json serialized bindings
+         * An array object bindings
          */
-        rawBindings: string[];
+        rawBindings: any[];
     }
 
-    export function registerFunction(funcMetadata: FunctionMetadata): void;
+    export type FunctionCallback = () => Promise<void>; // todo
+
+    export function registerFunction(funcMetadata: FunctionMetadata, callback: FunctionCallback): void;
 
     /**
      * Represents a type which can release resources, such as event listening or a timer.
@@ -22,25 +19,5 @@ declare module '@azure/functions-core' {
         static from(...disposableLikes: { dispose: () => any }[]): Disposable;
         constructor(callOnDispose: () => any);
         dispose(): any;
-    }
-
-    /**
-     * Register a hook to interact with the lifecycle of Azure Functions.
-     * Hooks are executed in the order they were registered and will block execution if they throw an error
-     */
-    export function registerHook(hookName: string, callback: HookCallback): Disposable;
-
-    export type HookCallback = (context: HookContext) => void | Promise<void>;
-
-    export type HookData = { [key: string]: any };
-
-    /**
-     * Base interface for all hook context objects
-     */
-    export interface HookContext {
-        /**
-         * The recommended place to share data between hooks
-         */
-        hookData: HookData;
     }
 }
