a1c61a5063916bf9b6e7abd0c3c012eba70ae809
Eric Jizba <erijiz@microsoft.com>
2022-08-11 14:16:52 -0700

    add comments

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { MyQueueItem } from "./models/MyQueueItem";
 // NOTE: This file and the code in the "functions" folder is the exact same code, just demonstrating different ways of organization
 // You can switch which one is used by changing the "main" field in "package.json" to either "dist/src/functions/*.js" or "dist/src/index.js"
 
+// A simple http trigger
 app.get('helloWorld', (context: InvocationContext, request: HttpRequest) => {
     context.log(`RequestUrl=${request.url}`);
 
@@ -12,6 +13,9 @@ app.get('helloWorld', (context: InvocationContext, request: HttpRequest) => {
     return { body: `Hello, ${name}!` };
 });
 
+// A more complex http trigger showing configuration options like:
+// - How to change trigger settings (authLevel & methods)
+// - How to add a secondary output to a storage queue
 const queueOutput = output.queue({ queueName: 'helloworldqueue', connection: 'storage_APPSETTING' });
 app.route('helloWorldQueue', {
     authLevel: "function",
@@ -28,6 +32,7 @@ app.route('helloWorldQueue', {
     }
 });
 
+// A simple storage queue trigger, triggered by the secondary output of `helloWorldQueue`
 app.queue('processQueueMessage', {
     queueName: 'helloworldqueue',
     connection: 'storage_APPSETTING',
@@ -36,6 +41,7 @@ app.queue('processQueueMessage', {
     }
 });
 
+// A simple timer trigger
 app.timer('snooze', {
     schedule: '0 */5 * * * *',
     handler: (context: InvocationContext, myTimer: Timer) => {
