a0e41b553303840306117d1d0601159efe6a2e70
Eric Jizba <erijiz@microsoft.com>
2022-08-11 14:16:06 -0700

    Add comments

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const { app, output } = require('@azure/functions');
 // NOTE: This file and the code in the "functions" folder is the exact same code, just demonstrating different ways of organization
 // You can switch which one is used by changing the "main" field in "package.json" to either "src/functions/*.js" or "src/index.js"
 
+// A simple http trigger
 app.get('helloWorld', async (context, request) => {
     context.log(`RequestUrl=${request.url}`);
 
@@ -11,6 +12,9 @@ app.get('helloWorld', async (context, request) => {
     return { body: `Hello, ${name}!` };
 });
 
+// A more complex http trigger showing configuration options like:
+// - How to change trigger settings (authLevel & methods)
+// - How to add a secondary output to a storage queue
 const queueOutput = output.queue({ queueName: 'helloworldqueue', connection: 'storage_APPSETTING' });
 app.route('helloWorldQueue', {
     authLevel: "function",
@@ -27,6 +31,7 @@ app.route('helloWorldQueue', {
     }
 });
 
+// A simple storage queue trigger, triggered by the secondary output of `helloWorldQueue`
 app.queue('processQueueMessage', {
     queueName: 'helloworldqueue',
     connection: 'storage_APPSETTING',
@@ -35,6 +40,7 @@ app.queue('processQueueMessage', {
     }
 });
 
+// A simple timer trigger
 app.timer('snooze', {
     schedule: '0 */5 * * * *',
     handler: async (context, myTimer) => {
