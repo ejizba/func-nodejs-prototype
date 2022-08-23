2af5082711705781d685bd9b64f3ae4757445ba8
Eric Jizba <erijiz@microsoft.com>
2022-08-23 12:35:37 -0700

    Get test file working again

--- test/functions/helloWorld.test.ts
+++ test/functions/helloWorld.test.ts
@@ -1,45 +1,59 @@
 import { HttpRequest, InvocationContext } from '@azure/functions';
 import { expect } from 'chai';
 import 'mocha';
-import { helloWorld } from '../../src/functions/helloWorld';
+import { helloWorld1 } from '../../src/functions/helloWorld1';
+
+function createTestInvocationContext(): InvocationContext {
+    return new InvocationContext({
+        functionName: 'testFunctionName',
+        invocationId: 'testInvocationId',
+        logHandler: (level, ...args) => {
+            switch (level) {
+                case 'error':
+                    console.error(...args);
+                    break;
+                case 'warning':
+                    console.warn(...args);
+                    break;
+                default:
+                    console.log(...args);
+            }
+        }
+    });
+}
+
 
 describe('helloWorld', () => {
     it('should return message with name from query', async () => {
-        const request: HttpRequest = {
-            headers: {},
+        const request = new HttpRequest({
             method: 'GET',
-            params: {},
             query: {
-                name: 'eric'
+                name: 'Eric'
             },
             url: 'http://localhost:7071/api/helloWorld'
-        };
-        const response = await helloWorld(new InvocationContext(), request);
+        });
+        const response = await helloWorld1(createTestInvocationContext(), request);
         expect(response.body).to.equal('Hello, Eric!');
     });
 
     it('should return message with name from body', async () => {
-        const request: HttpRequest = {
-            headers: {},
-            method: 'GET',
-            params: {},
-            query: {},
+        const request = new HttpRequest({
+            method: 'POST',
             url: 'http://localhost:7071/api/helloWorld',
-            body: 'Eric'
-        };
-        const response = await helloWorld(new InvocationContext(), request);
+            body: {
+                string: 'Eric'
+            }
+        });
+        const response = await helloWorld1(createTestInvocationContext(), request);
         expect(response.body).to.equal('Hello, Eric!');
     });
 
     it('should return message with no name', async () => {
-        const request: HttpRequest = {
-            headers: {},
+        const request = new HttpRequest({
             method: 'GET',
-            params: {},
-            query: {},
             url: 'http://localhost:7071/api/helloWorld'
-        };
-        const response = await helloWorld(new InvocationContext(), request);
+        });
+        const response = await helloWorld1(createTestInvocationContext(), request);
         expect(response.body).to.equal('Hello, world!');
     });
 })
