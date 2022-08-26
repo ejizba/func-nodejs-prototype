6bfa71b05983fd6121f0eaa62569cf4d7fb07208
Eric Jizba <erijiz@microsoft.com>
2022-08-26 16:22:29 -0700

    Improvements to unit tests

--- src/functions/helloWorld1.js
+++ src/functions/helloWorld1.js
@@ -1,12 +1,16 @@
 const { app } = require('@azure/functions');
 
+async function helloWorld1(context, request) {
+    context.log(`Http function processed request for url "${request.url}"`);
+
+    const name = request.query.get('name') || await request.text() || 'world';
+
+    return { body: `Hello, ${name}!` };
+};
+
 app.http('helloWorld1', {
     methods: ['GET', 'POST'],
-    handler: async (context, request) => {
-        context.log(`Http function processed request for url "${request.url}"`);
-
-        const name = request.query.get('name') || await request.text() || 'world';
-
-        return { body: `Hello, ${name}!` };
-    }
+    handler: helloWorld1
 });
+
+module.exports = { helloWorld1 };
