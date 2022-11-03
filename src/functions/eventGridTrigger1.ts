b7f0b0344a3b1b6211277aab26629fda35dc096c
hossam-nasr <hossam.nasr99@gmail.com>
2022-11-02 19:28:31 -0700

    remove eventGrid trigger

--- src/functions/eventGridTrigger1.ts
+++
@@ -1,9 +0,0 @@
-import { app, EventGridEvent, InvocationContext } from '@azure/functions';
-
-export async function eventGridTrigger1(context: InvocationContext, event: EventGridEvent): Promise<void> {
-    context.log('Event grid function processed event:', event);
-}
-
-app.eventGrid('eventGridTrigger1', {
-    handler: eventGridTrigger1
-});
