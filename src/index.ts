bbd5f1366f2782ca760079299ab161144b3a8176
hossam-nasr <hossam.nasr99@gmail.com>
2022-11-02 18:38:19 -0700

    clean up

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { app, HttpRequest, InvocationContext, Timer } from "@azure/functions";
+import { app, HttpRequest, InvocationContext, Timer } from '@azure/functions';
 
 app.get('httpTrigger1', async (context: InvocationContext, request: HttpRequest) => {
     context.log(`Http function processed request for url "${request.url}"`);
 
-    const name = request.query.get('name') || await request.text() || 'world';
+    const name = request.query.get('name') || (await request.text()) || 'world';
 
     return { body: `Hello, ${name}!` };
 });
@@ -13,5 +13,5 @@ app.timer('timerTrigger1', {
     handler: (context: InvocationContext, myTimer: Timer) => {
         var timeStamp = new Date().toISOString();
         context.log('The current time is: ', timeStamp);
-    } 
+    }
 });
