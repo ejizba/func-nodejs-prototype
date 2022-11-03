76cf4de279d5f03f11ea3f368b28c35bcb0401e8
hossam-nasr <hossam.nasr99@gmail.com>
2022-11-02 18:35:43 -0700

    format

--- src/functions/durableOrchestration.ts
+++ src/functions/durableOrchestration.ts
@@ -1,39 +1,25 @@
-import { HttpRequest, InvocationContext } from "@azure/functions";
-import * as df from "durable-functions";
-import {
-  DurableOrchestrationClient,
-  IOrchestrationFunctionContext,
-} from "durable-functions/lib/src/classes";
+import { HttpRequest, InvocationContext } from '@azure/functions';
+import * as df from 'durable-functions';
+import { DurableOrchestrationClient, IOrchestrationFunctionContext } from 'durable-functions/lib/src/classes';
 
-df.orchestration(
-  "DurableFunctionsOrchestratorJS",
-  function* (context: IOrchestrationFunctionContext) {
+df.orchestration('DurableFunctionsOrchestratorJS', function* (context: IOrchestrationFunctionContext) {
     const outputs = [];
-    outputs.push(yield context.df.callActivity("Hello", "Tokyo"));
-    outputs.push(yield context.df.callActivity("Hello", "Seattle"));
-    outputs.push(yield context.df.callActivity("Hello", "Cairo"));
+    outputs.push(yield context.df.callActivity('Hello', 'Tokyo'));
+    outputs.push(yield context.df.callActivity('Hello', 'Seattle'));
+    outputs.push(yield context.df.callActivity('Hello', 'Cairo'));
 
     return outputs;
-  }
-);
+});
 
-df.activity("Hello", (context: InvocationContext, name: string) => {
-  return `Hello, ${name}`;
+df.activity('Hello', (context: InvocationContext, name: string) => {
+    return `Hello, ${name}`;
 });
 
 df.httpClient(
-  "DurableFunctionsHttpStart",
-  async (
-    context: InvocationContext,
-    req: HttpRequest,
-    client: DurableOrchestrationClient
-  ) => {
-    const instanceId = await client.startNew(
-      req.query.get("functionName"),
-      undefined,
-      req.body
-    );
-    context.log(`Started orchestration with ID = '${instanceId}'.`);
-    return client.createCheckStatusResponse(req, instanceId);
-  }
+    'DurableFunctionsHttpStart',
+    async (context: InvocationContext, req: HttpRequest, client: DurableOrchestrationClient) => {
+        const instanceId = await client.startNew(req.query.get('functionName'), undefined, req.body);
+        context.log(`Started orchestration with ID = '${instanceId}'.`);
+        return client.createCheckStatusResponse(req, instanceId);
+    }
 );
