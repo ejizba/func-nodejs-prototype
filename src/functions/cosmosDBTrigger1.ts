56fbf2de8acbb98a5abbae55f34ab15f09405917
hossam-nasr <hossam.nasr99@gmail.com>
2022-11-04 16:38:22 -0700

    engineering changes

--- src/functions/cosmosDBTrigger1.ts
+++ src/functions/cosmosDBTrigger1.ts
@@ -1,4 +1,4 @@
-import { app, InvocationContext } from "@azure/functions";
+import { app, InvocationContext } from '@azure/functions';
 
 export async function cosmosDBTrigger1(context: InvocationContext, documents: unknown[]): Promise<void> {
     context.log(`Cosmos DB function processed ${documents.length} documents`);
@@ -11,7 +11,7 @@ if (process.env.cosmosDB_APPSETTING) {
         collectionName: 'helloWorldCol',
         partitionKey: '/id',
         createLeaseCollectionIfNotExists: true,
-        handler: cosmosDBTrigger1
+        handler: cosmosDBTrigger1,
     });
 } else {
     console.warn('Skipping registration of "cosmosDBTrigger1" because "cosmosDB_APPSETTING" is not defined');
--- src/functions/serviceBusTopicTrigger1.ts
+++ src/functions/serviceBusTopicTrigger1.ts
@@ -1,4 +1,4 @@
-import { app, InvocationContext } from "@azure/functions";
+import { app, InvocationContext } from '@azure/functions';
 
 export async function serviceBusTopicTrigger1(context: InvocationContext, message: unknown): Promise<void> {
     context.log('Service bus topic function processed message:', message);
@@ -9,7 +9,7 @@ if (process.env.serviceBus_APPSETTING) {
         connection: 'serviceBus_APPSETTING',
         topicName: 'helloWorldTopic',
         subscriptionName: 'helloWorldSub',
-        handler: serviceBusTopicTrigger1
+        handler: serviceBusTopicTrigger1,
     });
 } else {
     console.warn('Skipping registration of "serviceBusTopicTrigger1" because "serviceBus_APPSETTING" is not defined');
