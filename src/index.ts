176f91556ad37ecd99000ed73eecb7bf59a8aa83
hossam-nasr <hossam.nasr99@gmail.com>
2022-12-16 18:17:41 -0800

    testing

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
-import { app, HttpRequest, InvocationContext, Timer } from '@azure/functions';
+import {
+    app,
+    AppStartContext,
+    HttpRequest,
+    InvocationContext,
+    PostInvocationContext,
+    PreInvocationContext,
+    Timer,
+} from '@azure/functions';
 
 app.get('httpTrigger1', async (request: HttpRequest, context: InvocationContext) => {
     context.log(`Http function processed request for url "${request.url}"`);
@@ -6,6 +14,26 @@ app.get('httpTrigger1', async (request: HttpRequest, context: InvocationContext)
     const name = request.query.get('name') || (await request.text()) || 'world';
 
     return { body: `Hello, ${name}!` };
+});
+
+app.get('httpTrigger2', async (context: InvocationContext, request: HttpRequest) => {
+    context.log(`Http function processed request for url "${request.url}"`);
+
+    const name = request.query.get('name') || (await request.text()) || 'world';
+
+    return { body: `Hello, ${name}!` };
+});
+
+app.onStart((context: AppStartContext) => {
+    console.log(`App start woooooo. Function dir is ${context.functionAppDirectory}`);
+});
+
+app.onPreInvocation(['httpTrigger1'], (context: PreInvocationContext) => {
+    context.invocationContext.log(`pre invocation for ${context.invocationContext.functionName}`);
+});
+
+app.onPostInvocation([], (context: PostInvocationContext) => {
+    context.invocationContext.log(`post invocation for ${context.invocationContext.functionName}`);
 });
 
 app.timer('timerTrigger1', {
