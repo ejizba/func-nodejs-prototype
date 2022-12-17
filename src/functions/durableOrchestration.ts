802b67526240cc6f824c547aaa59f22c6a1cd699
Hossam Mabed <31644556+hossam-nasr@users.noreply.github.com>
2022-12-16 18:09:40 -0800

    Add durable orchestration and entity examples (#14)
    * add durable prototype
    
    * local tests
    
    * add durable orchestration and counter
    
    * format
    
    * clean up
    
    * more clean up
    
    * add rimraf
    
    * switch back main
    
    * remove prettier config file
    
    * separate function definitions
    
    * structure
    
    * name => input
    
    * req -> request
    
    * body -> text()
    
    * add get
    
    * naming
    
    * organization
    
    * fix route
    
    * hello naming
    
    * update types
    
    * revert rimraf
    
    * add complex example
    
    * update APIs
    
    * remove df.client()
    
    * rename to httpStart
    
    * rename to durableClient
    
    * add app namespace
    
    * remove generics from activity
    
    * update package version
    
    * merge conflicts
    
    * use the new HttpResponse class
    
    * rename entityHandler

---
+++ src/functions/durableOrchestration.ts
@@ -0,0 +1,36 @@
+import { app, HttpHandler, HttpRequest, InvocationContext } from '@azure/functions';
+import * as df from 'durable-functions';
+import { ActivityHandler, OrchestrationHandler } from 'durable-functions';
+
+// Replace with the name of your Durable Functions Activity
+const activityName = 'hello';
+
+const orchestrator: OrchestrationHandler = function* (context) {
+    const outputs = [];
+    outputs.push(yield context.df.callActivity(activityName, 'Tokyo'));
+    outputs.push(yield context.df.callActivity(activityName, 'Seattle'));
+    outputs.push(yield context.df.callActivity(activityName, 'Cairo'));
+
+    return outputs;
+};
+df.app.orchestration('durableOrchestrator1', orchestrator);
+
+const helloActivity: ActivityHandler = (input: string) => {
+    return `Hello, ${input}`;
+};
+df.app.activity(activityName, { handler: helloActivity });
+
+const clientInput = df.input.durableClient();
+
+const httpStart: HttpHandler = async (request: HttpRequest, context: InvocationContext) => {
+    const client = df.getClient(context, clientInput);
+    const instanceId = await client.startNew(request.params.orchestratorName, undefined, request.text());
+    context.log(`Started orchestration with ID = '${instanceId}'.`);
+    return client.createCheckStatusResponse(request, instanceId);
+};
+
+app.http('durableOrchestrationStart1', {
+    route: 'orchestrators/{orchestratorName}',
+    extraInputs: [clientInput],
+    handler: httpStart,
+});
