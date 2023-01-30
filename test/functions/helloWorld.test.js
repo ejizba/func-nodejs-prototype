cd5239fc0ac7a0dba3b3a8e595c5a0601e96b833
Eric Jizba <erijiz@microsoft.com>
2023-01-30 13:55:08 -0800

    Bump func package

--- test/functions/helloWorld.test.js
+++ test/functions/helloWorld.test.js
@@ -3,26 +3,6 @@ const { expect } = require('chai');
 require('mocha');
 const { helloWorld1 } = require('../../src/functions/helloWorld1');
 
-function createTestInvocationContext() {
-    return new InvocationContext({
-        functionName: 'testFunctionName',
-        invocationId: 'testInvocationId',
-        logHandler: (level, ...args) => {
-            switch (level) {
-                case 'error':
-                    console.error(...args);
-                    break;
-                case 'warning':
-                    console.warn(...args);
-                    break;
-                default:
-                    console.log(...args);
-            }
-        }
-    });
-}
-
-
 describe('helloWorld', () => {
     it('should return message with name from query', async () => {
         const request = new HttpRequest({
@@ -32,7 +12,7 @@ describe('helloWorld', () => {
             },
             url: 'http://localhost:7071/api/helloWorld'
         });
-        const response = await helloWorld1(request, createTestInvocationContext());
+        const response = await helloWorld1(request, new InvocationContext());
         expect(response.body).to.equal('Hello, Eric!');
     });
 
@@ -44,7 +24,7 @@ describe('helloWorld', () => {
                 string: 'Eric'
             }
         });
-        const response = await helloWorld1(request, createTestInvocationContext());
+        const response = await helloWorld1(request, new InvocationContext());
         expect(response.body).to.equal('Hello, Eric!');
     });
 
@@ -53,7 +33,7 @@ describe('helloWorld', () => {
             method: 'GET',
             url: 'http://localhost:7071/api/helloWorld'
         });
-        const response = await helloWorld1(request, createTestInvocationContext());
+        const response = await helloWorld1(request, new InvocationContext());
         expect(response.body).to.equal('Hello, world!');
     });
 })
