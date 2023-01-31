b0bcc0ba658d28bee044856b99560212e7435f5c
hossam-nasr <hossam.nasr99@gmail.com>
2023-01-31 15:28:05 -0800

    make changes

--- src/functions/durableEntity.ts
+++ src/functions/durableEntity.ts
@@ -1,35 +1,11 @@
 import { app, HttpHandler, HttpRequest, HttpResponse, InvocationContext } from '@azure/functions';
 import * as df from 'durable-functions';
-import { EntityHandler } from 'durable-functions';
+import { EntityContext, EntityHandler } from 'durable-functions';
 
 // Replace with your own Durable entity name
 const entityName = 'Counter';
 
-const clientInput = df.input.durableClient();
-
-const httpStart: HttpHandler = async (req: HttpRequest, context: InvocationContext) => {
-    const id: string = req.params.id;
-    const entityId = new df.EntityId(entityName, id);
-    const client = df.getClient(context, clientInput);
-
-    if (req.method === 'POST') {
-        // increment value
-        await client.signalEntity(entityId, 'add', 1);
-    } else {
-        // read current state of entity
-        const stateResponse = await client.readEntityState(entityId);
-        return new HttpResponse({
-            jsonBody: stateResponse.entityState,
-        });
-    }
-};
-app.http('durableEntityStart1', {
-    route: `${entityName}/{id}`,
-    extraInputs: [clientInput],
-    handler: httpStart,
-});
-
-const counterEntity: EntityHandler<number> = (context) => {
+const counterEntity: EntityHandler<number> = (context: EntityContext<number>) => {
     const currentValue: number = context.df.getState(() => 0);
     switch (context.df.operationName) {
         case 'add':
@@ -45,3 +21,25 @@ const counterEntity: EntityHandler<number> = (context) => {
     }
 };
 df.app.entity(entityName, counterEntity);
+
+const httpStart: HttpHandler = async (req: HttpRequest, context: InvocationContext): Promise<HttpResponse> => {
+    const id: string = req.params.id;
+    const entityId = new df.EntityId(entityName, id);
+    const client = df.getClient(context);
+
+    if (req.method === 'POST') {
+        // increment value
+        await client.signalEntity(entityId, 'add', 1);
+    } else {
+        // read current state of entity
+        const stateResponse = await client.readEntityState(entityId);
+        return new HttpResponse({
+            jsonBody: stateResponse.entityState,
+        });
+    }
+};
+app.http('durableEntityStart1', {
+    route: `${entityName}/{id}`,
+    extraInputs: [df.input.durableClient()],
+    handler: httpStart,
+});
